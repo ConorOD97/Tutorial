1295f3cf044dc210bd450c7375c25c4e2ec5f64c
Conor ODonnell <c17441552@dit.ie>
2020-01-30 09:56:26 +0000

    test branch stuff

--- src/ie/tudublin/Main.java
+++ src/ie/tudublin/Main.java
@@ -1,8 +1,11 @@
 package ie.tudublin; //prevents namespace conflicts
 //gitignore ignores files that you dont want to go into the git repo when you commit 
-
+//git add . adds all files to staging area before you make a commit
 public class Main
 {
+
+    //changes on the test branch
+    
     public static void main(String[] args)
     {
         for (int i =0;i <args.length; i++){
